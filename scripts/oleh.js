aefcf07e8af0d533a406bb0e49117d3f895bb2dc
olehfff <olegsamilenko5@gmail.com>
2024-05-23 19:55:36 +0300

    update

--- scripts/oleh.js
+++ scripts/oleh.js
@@ -4,5 +4,7 @@ console.log(buttonPlayAgain)
 const numberGess = document.querySelector(".number");
 console.log(numberGess)
 
-function getRandomNumber() {return Math.round(Math.random() * (10 - 1) + 1)}
+function getRandomNumber() {
+    return Math.round(Math.random() * (10 - 1) + 1)
+}
 getRandomNumber()
