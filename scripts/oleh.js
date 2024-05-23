1db74bc0b36c0e167dc5b4eee66884ccee5624cb
olehfff <olegsamilenko5@gmail.com>
2024-05-23 18:37:19 +0300

    add : index.html, oleh.css, oleh.js

--- scripts/oleh.js
+++ scripts/oleh.js
@@ -1,1 +1,8 @@
-console.log('Oleh');
+const buttonPlayAgain = document.querySelector(".button");
+console.log(buttonPlayAgain)
+
+const numberGess = document.querySelector(".number");
+console.log(numberGess)
+
+function getRandomNumber() {return Math.round(Math.random() * (10 - 1) + 1)}
+getRandomNumber()
